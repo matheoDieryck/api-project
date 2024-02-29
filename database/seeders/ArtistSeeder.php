9f8c20e83f37209993f405fd6833ad560d4c1a2d
matheoDieryck <matheo.dieryck@student.vives.be>
2024-02-29 15:07:33 +0100

    main

--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Artist;
 class ArtistSeeder extends Seeder
 {
     /**
