d4ee482f19007b5612cb6c74f22feda6500b485f
matheoDieryck <matheo.dieryck@student.vives.be>
2024-02-29 15:05:01 +0100

    mainmainx

---
+++ database/seeders/ArtistSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArtistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Artist::factory()->count(50)->create();
+    }
+}
